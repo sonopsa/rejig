1a62e6b79bf41925da17a7419273d1bd9ba1fe84
sonopsa <32681149+sonopsa@users.noreply.github.com>
2024-04-11 16:58:39 -0700

    add WorldLoadEvent

---
+++ src/main/java/com/github/sonopsa/rejig/mixin/event/WorldLoadEvent.java
@@ -0,0 +1,16 @@
+package com.github.sonopsa.rejig.mixin.event;
+
+import com.github.sonopsa.rejig.Rejig;
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class WorldLoadEvent {
+    @Inject(at = @At("TAIL"), method = "loadWorld")
+    private void init(CallbackInfo info) {
+        Rejig.onWorldLoad();
+    }
+}
