e7fa1fc64c46372b08b33261d8ec9eacadc82ed5
sonopsa <32681149+sonopsa@users.noreply.github.com>
2024-06-01 20:46:59 -0700

    update server only

--- src/client/java/com/github/sonopsa/rejig/accessor/LightPipelineProviderAccess.java
+++
@@ -1,7 +0,0 @@
-package com.github.sonopsa.rejig.accessor;
-
-import me.jellysquid.mods.sodium.client.model.light.LightPipeline;
-
-public interface LightPipelineProviderAccess {
-    LightPipeline rejig$getGlowingLightPipeline();
-}
